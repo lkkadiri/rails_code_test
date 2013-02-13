1d688146846d69332e63d325f31bfb25264056ae
Huddle Room 522 <huddleroom522@sane-mac-mini.local>
2013-02-13 13:22:29 -0700

    First commit with the new rails project with it

---
+++ code_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeTest::Application.config.secret_token = '131a42bf0705f1dd9c8b5038dfb99a1d2cf3fd0f773a2fa09b2953683a39ba1e9f0893f58e7e355584859560b0b381184bfd058917ce5cb3a883a2a212187dfd'
